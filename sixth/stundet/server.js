8de88b2359fbe1e1d61ad5776d73360e5be278d7
zhufengzhufeng <894918097@qq.com>
2016-10-19 15:22:42 +0800

    delete操作

--- sixth/stundet/server.js
+++ sixth/stundet/server.js
@@ -18,7 +18,7 @@ http.createServer(function (req,res) {
         switch (req.method){
             case 'GET':
                 if(id){//获取一个
-
+                    
                 }else {
                     res.end(JSON.stringify(students));
                 }
